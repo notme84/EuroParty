fbfbdd341d492760fc8dc8017fe30fab7818a658
notme84 <nguster@gmail.com>
2024-09-23 22:50:16 -0700

    starting board scene
    4 worker "spaces" + mouse over

---
+++ Scenes/background.gd
@@ -0,0 +1,11 @@
+extends Sprite2D
+
+
+func _on_area_2d_mouse_entered():
+	set_modulate(Color.RED)
+	pass # Replace with function body.
+
+
+func _on_area_2d_mouse_exited():
+	set_modulate(Color.WHITE)
+	pass # Replace with function body.
---
+++ Scenes/water_space.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+func _on_area_2d_mouse_entered():
+	pass # Replace with function body.
+
+
+func _on_area_2d_mouse_exited():
+	pass # Replace with function body.
